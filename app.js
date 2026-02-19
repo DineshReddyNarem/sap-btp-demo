651c7d7887a9755a68ca4fd85a45cfc69d363fa7
Dinesh Reddy Narem <dineshreddynarem@gmail.com>
2026-02-19 16:41:28 +0000

    second push

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from SAP BTP CI/CD Demo!demo1"
+    message: "Hello from SAP BTP CI/CD Demo!demo1secondpush"
   });
 });
 app.listen(PORT, () => {
